33055b5d7818f36856d3ec70d102e80257447fa3
Irene <ireneisley17@gmail.com>
2024-03-18 15:01:36 -0300

    commit

---
+++ frontend/src/pages/Mapas.jsx
@@ -0,0 +1,258 @@
+import { Flex, Box, Center, FormControl, HStack, Button, Image } from "@chakra-ui/react";
+import { useHistory } from 'react-router-dom';
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+import React from 'react';
+
+function Mapas() {
+  const history = useHistory();
+
+  const { user, logOut } = useContext(UserContext);
+
+
+
+  const handleCadastroFuncionarios = () => {
+    history.push('/cadastro-funcionario')
+  }
+
+  const handleComanda = () => {
+    history.push('/gestao-comandas')
+  }
+
+  const handleFinanceiro = () => {
+    history.push('/financeiro')
+  }
+
+  const handleGestaoUsuarios = () => {
+    history.push('/gestao-usuarios')
+  }
+
+  const handleAgendamentos = () => {
+    history.push('/agendamentos')
+  }
+
+  const handleAgendar = () => {
+    history.push('/')
+  }
+
+
+  return (
+    <Box h="100vh" position="relative">
+      {/* Logo */}
+      <Image 
+        src="/queimadastour.png" 
+        alt="Logo" 
+        h={{ base: 20, md: 32 }}
+        maxW="100%"
+        position="absolute" 
+        top={{ base: 4, md: 2}} 
+        left={4} 
+        zIndex={1} 
+      />
+
+      {/* Botão de Sair */}
+      <Button
+        w={{ base: 90, md: 200 }}
+        p="6"
+        type="button"
+        bg="#E68854"
+        color="white"
+        fontWeight="bold"
+        fontSize="xl"
+        mt="2"
+        ml="2"
+        _hover={{ bg: "red.500" }}
+        h={{ base: 50, md: "auto"}}
+        position="absolute"
+        top={6}
+        right={2}
+        zIndex={1}
+        onClick={logOut}
+      >
+        Sair
+      </Button>
+
+      {/* Título para telas grandes */}
+      <Center
+        as="header"
+        h={140}
+        bg="#E68854"
+        color="white"
+        fontWeight="bold"
+        fontSize={{ base: '2xl', md: '4xl' }}
+        pb="8"
+      >
+        Administração
+      </Center>
+
+      {/* Conteúdo central responsivo */}
+      <Flex
+        align="center"
+        justify="center"
+        bg="blackAlpha.200"
+        h="calc(100vh - 150px)"
+      >
+        <Center
+          w="100%"
+          maxW={840}
+          bg="white"
+          top={{ base: 50, md: '100px' }}
+          position="absolute"
+          borderRadius={5}
+          p="6"
+          boxShadow="0 1px 2px #ccc"
+          mt={{ base: 50, md: 0 }}
+        >
+          <FormControl 
+            display="flex" 
+            flexDir="column" 
+            gap="4"
+          >
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854" 
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleAgendar }
+              >
+                Controle de Mapa
+              </Button>
+            </HStack>
+            
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854"
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleCadastroFuncionarios }
+              >
+                Cadastro de Funcionários
+              </Button>
+            </HStack>
+
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854"
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleComanda }
+              >
+                Descrição de Viagem
+              </Button>
+            </HStack>
+
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854"
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleFinanceiro }
+              >
+                Financeiro
+              </Button>
+            </HStack>
+
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854" 
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleGestaoUsuarios }
+              >
+                Edição de Página
+              </Button>
+            </HStack>
+
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854"
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleAgendamentos }
+              >
+                Ticket
+              </Button>
+            </HStack>
+
+            <HStack spacing="4" justify="center">
+              <Button
+                w={240}
+                p="6"
+                type="submit"
+                bg="#E68854" 
+                color="white"
+                fontWeight="bold"
+                fontSize="xl"
+                mt="2"
+                ml="2"
+                _hover={{ bg: "gray.900" }}
+                h="auto"
+                onClick={ handleAgendar }
+              >
+                Vendas
+              </Button>
+            </HStack>
+
+          
+
+            {/* <HStack justify="center">
+              <Flex justify="space-between">
+
+              </Flex>
+            </HStack> */}
+          </FormControl>
+        </Center>
+      </Flex>
+    </Box>
+  );
+}
+
+export default Mapas;
--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,3 +1,6 @@
+// routes.js (ou onde quer que você defina suas rotas)
+
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import Admin from '../pages/Admin';
@@ -15,29 +18,30 @@ import Cabeleireiro from '../pages/Cabeleireiro';
 import GestaoComandas from '../pages/GestaoComandas';
 import EditarComandas from '../pages/EditarComandas';
 import CadastroFuncionario from '../pages/CadastroFuncionario';
+import Mapas from '../pages/Mapas';
 
 function Routes() {
-
   return (
     <Switch>
-      <Route exact path="/login" component={ Login }/>
-      <Route exact path="/administrador" component={ Admin }/>
-      <Route exact path="/barbeiro" component={ Barbeiros }/>
-      <Route exact path="/manicure" component={ Manicure }/>
-      <Route exact path="/cabeleireiro" component={ Cabeleireiro }/>
-      <Route exact path="/cadastro-funcionario" component={ CadastroFuncionario }/>
-      <Route exact path="/" component={ Agendar }/>
-      <Route exact path="/comanda" component={ Comanda }/>
-      <Route exact path="/financeiro" component={ Financeiro }/>
-      <Route exact path="/gestao-usuarios" component={ GestaoUsuarios }/>
-      <Route exact path="/agendamentos" component={ Agendamentos }/>
-      <Route exact path="/agendamentos-funcionarios/:id" component={ AgendamentosBarbeiros }/>
-      <Route exact path="/gestao-comandas" component={ GestaoComandas }/>
-      <Route exact path="/editar-agendamento/:id" component={ EditarAgendamento }/>
-      <Route exact path="/editar-usuarios/:id" component={ EditarUsuario }/>
-      <Route exact path="/editar-comandas/:id" component={ EditarComandas }/>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/administrador" component={Admin} />
+      <Route exact path="/barbeiro" component={Barbeiros} />
+      <Route exact path="/manicure" component={Manicure} />
+      <Route exact path="/cabeleireiro" component={Cabeleireiro} />
+      <Route exact path="/cadastro-funcionario" component={CadastroFuncionario} />
+      <Route exact path="/" component={Agendar} />
+      <Route exact path="/comanda" component={Comanda} />
+      <Route exact path="/financeiro" component={Financeiro} />
+      <Route exact path="/gestao-usuarios" component={GestaoUsuarios} />
+      <Route exact path="/agendamentos" component={Agendamentos} />
+      <Route exact path="/agendamentos-funcionarios/:id" component={AgendamentosBarbeiros} />
+      <Route exact path="/gestao-comandas" component={GestaoComandas} />
+      <Route exact path="/editar-agendamento/:id" component={EditarAgendamento} />
+      <Route exact path="/editar-usuarios/:id" component={EditarUsuario} />
+      <Route exact path="/editar-comandas/:id" component={EditarComandas} />
+      <Route exact path="/mapas" component={Mapas} /> 
     </Switch>
   )
 }
 
-export default Routes
+export default Routes;
